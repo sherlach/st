80d1dc394a818666acb8bb339c35eeef5629852a
sherlach <68379649+sherlach@users.noreply.github.com>
2021-08-24 10:21:40 +1000

    updated docs

--- config.mk
+++ config.mk
@@ -4,8 +4,8 @@ VERSION = 0.8.4
 # Customize below to fit your system
 
 # paths
-#PREFIX = /usr/local
 PREFIX = /usr
+#PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
 X11INC = /usr/X11R6/include
