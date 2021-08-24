2328c802a602fa35ff2172d44e3130cbec724975
sherlach <68379649+sherlach@users.noreply.github.com>
2021-08-24 11:34:49 +1000

    added final patch, boxdraw

--- st.h
+++ st.h
@@ -35,7 +35,8 @@ enum glyph_attribute {
 	ATTR_WRAP       = 1 << 8,
 	ATTR_WIDE       = 1 << 9,
 	ATTR_WDUMMY     = 1 << 10,
-  ATTR_LIGA       = 1 << 11,
+  ATTR_BOXDRAW    = 1 << 11,
+  ATTR_LIGA       = 1 << 12,
 	ATTR_BOLD_FAINT = ATTR_BOLD | ATTR_FAINT,
 };
 
@@ -120,6 +121,15 @@ void *xmalloc(size_t);
 void *xrealloc(void *, size_t);
 char *xstrdup(const char *);
 
+int isboxdraw(Rune);
+ushort boxdrawindex(const Glyph *);
+#ifdef XFT_VERSION
+/* only exposed to x.c, otherwise we'll need Xft.h for the types */
+void boxdraw_xinit(Display *, Colormap, XftDraw *, Visual *);
+void drawboxes(int, int, int, int, XftColor *, XftColor *, const XftGlyphFontSpec *, int);
+#endif
+
+
 /* config.h globals */
 extern char *utmp;
 extern char *scroll;
@@ -133,4 +143,5 @@ extern unsigned int tabspaces;
 extern unsigned int defaultfg;
 extern unsigned int defaultbg;
 extern unsigned int defaultcs;
-extern float alpha, alphaUnfocused; // XXX is this consistent style?
+extern const int boxdraw, boxdraw_bold, boxdraw_braille;
+extern float alpha, alphaUnfocused; // XXX is this cap consistent style?
